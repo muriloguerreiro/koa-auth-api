af6cbd6c6b2418f5c89919a7570f0d155490e817
murilo.badoco <guerreiro_murilo@hotmail.com>
2024-11-04 17:48:17 -0300

    create: add data-source configuration and initialize db connection

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,18 @@
 import Koa from 'koa';
+import { AppDataSource } from './data-source';
 
 const app = new Koa();
 
-app.use(async (ctx) => {
-    ctx.body = 'Hello, Koa!';
-});
+AppDataSource.initialize()
+    .then(() => {
+        console.log("Database connected successfully!");
 
-app.listen(3000, () => {
-    console.log('Server is running on http://localhost:3000');
-});
+        app.use(async (ctx) => {
+            ctx.body = 'Hello, Koa!';
+        });
+
+        app.listen(3000, () => {
+            console.log('Server is running on http://localhost:3000');
+        });
+    })
+    .catch((error) => console.log("Fail connecting to database!", error));
